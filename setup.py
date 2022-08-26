452bb8889053f7161d5ecb70673a707232b911fe
Giovanni L <callmegiorgio@hotmail.com>
2022-08-26 16:50:59 -0300

    created setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='pyqt5_fugueicons',
+    version='3.5.6.0',
+    description='Fugue Icons for PyQt5',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Giovanni L',
+    author_email='callmegiorgio@hotmail.com',
+    url='https://github.com/callmegiorgio/pyqt5_fugueicons',
+    license='MIT',
+    packages=['pyqt5_fugueicons'],
+    keywords=['pyqt5', 'icons'],
+
+    install_requires=['PyQt5'],
+
+    python_requires='>=3.6'
+)
